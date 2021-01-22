6744efd9f90320f514e9bbe4a759834feea833d3
Briva99 <bang.briva@gmail.com>
2021-01-23 04:28:00 +0700

    masih belum sempurna

---
+++ src/Language.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+class Language extends React.Component {
+    render() {
+        return (
+            <div className='item-soal'>
+                <div className='nama-soal'>{this.props.soal}</div>
+                    if (type==='button'){
+
+                    }else if(type==='checkBox'){
+
+                    } else (type==='essy'){
+
+                    }
+
+                {/* <div className='form'>
+                    <form>
+                        <textarea type={this.props.type}/>
+                    </form>
+                </div> */}
+
+            </div>
+        )
+    }
+}
+
+export default Language;
